8ddcf8e09259c05c12c83c9398e05e5e78ec0d65
Gabriel Zanlorenssi <gabriel.zanlorenssi@nexojornal.com.br>
2018-03-16 20:08:15 -0300

    updated code

--- codigo.r
+++ codigo.r
@@ -2,19 +2,20 @@ library(dplyr)
 library(ggplot2)
 library(ggthemes)
 library(readxl)
+libary(tidyr)
 
 candidatos <- readxls("candidatos.xls")
 
 candidatos %>% 
   gather(var, val, `1`:`43`) %>% 
   mutate(val = ifelse(is.na(val), 0, val),
-         `Número de posicionamentos` = as.factor(val),
+         `NÃºmero de posicionamentos` = as.factor(val),
          var = as.numeric(as.character(var))) %>% 
-  ggplot(aes(y=reorder(Candidato, desc(Candidato)), x=var, fill=`Número de posicionamentos`)) +
+  ggplot(aes(y=reorder(Candidato, desc(Candidato)), x=var, fill=`NÃºmero de posicionamentos`)) +
   geom_tile(color = "gray") +
   scale_fill_manual(values=c("#d9d9d9", "#d7301f", "#b30000", "#7f0000")) +
   theme_fivethirtyeight() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   theme(axis.title = element_text(), axis.title.y = element_blank()) +
-  labs(x="Horas após a primeira notícia da morte de Marielle", y="Pré-Candidato", title="Quanto tempo os pré-Candidatos demoraram para se posicionar sobre Marielle?",
-       subtitle="Contando apenas posts no Facebook que fazem referência direta a Marielle") 
+  labs(x="Horas apÃ³s a primeira notÃ­cia da morte de Marielle", y="PrÃ©-Candidato", title="Quanto tempo os prÃ©-Candidatos demoraram para se posicionar sobre Marielle?",
+       subtitle="Contando apenas posts no Facebook que fazem referÃªncia direta a Marielle") 
